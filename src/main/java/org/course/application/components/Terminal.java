8141372c4165a85e9fb43bfd49c100421a0a8519
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-23 02:14:23 +0300

    add Terminal

---
+++ src/main/java/org/course/application/components/Terminal.java
@@ -0,0 +1,32 @@
+package org.course.application.components;
+
+import org.course.application.Order;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static org.course.statistic.StatController.lambda;
+
+public class Terminal {
+  @NotNull
+  final private Buffer buffer;
+  @NotNull
+  List<Client> clients;
+
+  public Terminal(@NotNull final Buffer buffer, @NotNull final List<Client> clients) {
+    this.buffer = buffer;
+    this.clients = clients;
+  }
+
+  public void addOrderToBuffer(@NotNull final Order order) {
+    buffer.addOrder(order);
+  }
+
+  public double getNextOrderGenerationTime() {
+    return (-1.0 / lambda) * Math.log(Math.random());
+  }
+
+  public boolean isExistOrders() {
+    return !buffer.isEmpty();
+  }
+}
