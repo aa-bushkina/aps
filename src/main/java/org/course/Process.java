19dde906348d7e2b2b54922d9caa25af59ef2d36
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-24 07:56:35 +0300

    rename main to process

--- src/main/java/org/course/Process.java
+++ src/main/java/org/course/Process.java
@@ -2,7 +2,7 @@ package org.course;
 
 import org.course.GUI.UI;
 
-public class Main {
+public class Process {
   public static void main(String[] args) throws InterruptedException {
     final UI ui = new UI();
     ui.execute();
