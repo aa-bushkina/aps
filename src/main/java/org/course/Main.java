4995f3d4dc56724654e302c20479320c5177a25c
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-24 04:04:31 +0300

    add Main class

---
+++ src/main/java/org/course/Main.java
@@ -0,0 +1,10 @@
+package org.course;
+
+import org.course.GUI.UI;
+
+public class Main {
+  public static void main(String[] args) throws InterruptedException {
+    final UI ui = new UI();
+    ui.execute();
+  }
+}
