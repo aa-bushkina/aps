16028cde7bc14a77a11ffc81ff1f6a1ab00a14bb
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-25 11:17:46 +0300

    replace fields

--- src/main/java/org/course/application/components/RestaurantDevice.java
+++ src/main/java/org/course/application/components/RestaurantDevice.java
@@ -3,22 +3,18 @@ package org.course.application.components;
 import lombok.Getter;
 import lombok.Setter;
 import org.course.application.Order;
-
-import java.util.Random;
+import org.jetbrains.annotations.NotNull;
 
 @Getter
 @Setter
 public class RestaurantDevice {
-
   private int deviceId;
   private Order currentOrder;
   private double orderStartTime;
-  private Random generator;
-  private double maxRand;
 
   public RestaurantDevice(final int deviceId) {
-    this.orderStartTime = 0;
     this.deviceId = deviceId;
+    this.orderStartTime = 0;
     this.currentOrder = null;
   }
 
